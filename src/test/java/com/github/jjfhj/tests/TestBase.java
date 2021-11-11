effc12cc6f21e483afae0999d67b658ce1c952b3
Karina <aiwpront@gmail.com>
2021-11-11 16:27:13 +0400

    updated TestBase.java

--- src/test/java/com/github/jjfhj/tests/TestBase.java
+++ src/test/java/com/github/jjfhj/tests/TestBase.java
@@ -1,9 +1,13 @@
 package com.github.jjfhj.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.github.jjfhj.pages.RegistrationsPage;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
+    RegistrationsPage registrationsPage = new RegistrationsPage();
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
