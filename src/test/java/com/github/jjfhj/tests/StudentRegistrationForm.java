414e44bbc67c8f0920c9207ab795c40456912453
Karina <aiwpront@gmail.com>
2021-11-11 23:07:15 +0400

    added Page Object

--- src/test/java/com/github/jjfhj/tests/StudentRegistrationForm.java
+++ src/test/java/com/github/jjfhj/tests/StudentRegistrationForm.java
@@ -2,69 +2,37 @@ package com.github.jjfhj.tests;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class StudentRegistrationForm extends TestBase {
 
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/automation-practice-form");
-
-        //Заполнение полей First Name и Last Name
-        $("#firstName").setValue("Carol");
-        $("#lastName").setValue("Delmonte");
-
-        //Заполнение поля Email
-        $("#userEmail").setValue("CarolBDelmonte@armyspy.com");
-
-        //Выбор пола в поле Gender
-        $("[for=\"gender-radio-1\"]").click();
-
-        //Заполнение поля Mobile
-        $("#userNumber").setValue("8165059611");
-
-        //Выбор даты рождения в поле Date of Birth
-        $("#dateOfBirthInput").click();
-        $("[class=\"react-datepicker__year-select\"]").$("[value=\"1988\"]").click();
-        $("[class=\"react-datepicker__month-select\"]").$("[value=\"7\"]").click();
-        $("[class=\"react-datepicker__day react-datepicker__day--016\"]").click();
-
-        //Заполнение поля Subjects
-        $("#subjectsInput").setValue("Hindi").pressEnter();
-
-        //Выбор хобби в поле Hobbies
-        $("[for=\"hobbies-checkbox-2\"]").click();
-        $("[for=\"hobbies-checkbox-3\"]").click();
-
-        //Загрузка изображения
-        $("#uploadPicture").uploadFile(new File("src/test/resources/Duck_on_Yeadon_Tarn.jpg"));
-
-        //Заполнение поля Current Address
-        $("#currentAddress").setValue("4959 Tree Frog Lane\n" + "Kansas City, MO 64151");
-
-        //Заполнение полей State и City
-        $("#react-select-3-input").setValue("Haryana").pressEnter();
-        $("#react-select-4-input").setValue("Panipat").pressEnter();
-
-        //Нажатие кнопки Submit
-        $("#submit").click();
-
-        //Проверка всплывающего модального окна после успешной отправки формы
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $("[class=\"table table-dark table-striped table-bordered table-hover\"]").$("tbody")
-                .shouldHave(text("Carol Delmonte"),
-                        text("CarolBDelmonte@armyspy.com"),
-                        text("Male"),
-                        text("8165059611"),
-                        text("16 August,1988"),
-                        text("Hindi"),
-                        text("Reading, Music"),
-                        text("Duck_on_Yeadon_Tarn.jpg"),
-                        text("4959 Tree Frog Lane\n" + "Kansas City, MO 64151"),
-                        text("Haryana Panipat"));
+        registrationsPage.openPage()
+                //Заполнить форму регистрации "Student Registration Form"
+                .typeFirstName("Carol")
+                .typeLastName("Delmonte")
+                .typeEmail("CarolBDelmonte@armyspy.com")
+                .selectGender("Male")
+                .typeNumber("8165059611")
+                .setDate("16", "August", "1988")
+                .typeSubjects("Hindi")
+                .selectHobbies("Reading")
+                .selectHobbies("Music")
+                .uploadFile("Duck_on_Yeadon_Tarn.jpg")
+                .typeCurrentAddress("4959 Tree Frog Lane\n" + "Kansas City, MO 64151")
+                .selectState("Haryana")
+                .selectCity("Panipat")
+                .clickSubmit()
+                //Проверить всплывающее модальное окно "Thanks for submitting the form" после успешной отправки формы
+                .checkModalFormTitle()
+                .checkModalFormValue("Student Name", "Carol Delmonte")
+                .checkModalFormValue("Student Email", "CarolBDelmonte@armyspy.com")
+                .checkModalFormValue("Gender", "Male")
+                .checkModalFormValue("Mobile", "8165059611")
+                .checkModalFormValue("Date of Birth", "16 August,1988")
+                .checkModalFormValue("Subjects", "Hindi")
+                .checkModalFormValue("Hobbies", "Reading, Music")
+                .checkModalFormValue("Picture", "Duck_on_Yeadon_Tarn.jpg")
+                .checkModalFormValue("Address", "4959 Tree Frog Lane\n" + "Kansas City, MO 64151")
+                .checkModalFormValue("State and City", "Haryana Panipat");
     }
 }
